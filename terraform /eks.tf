ee738b80d1510fa837c411653834e3337e053dde
eslam sabik <eslam@machine1.eslam.com>
2026-02-27 23:50:37 +0200

    Final pipeline with all fixes

--- terraform /eks.tf
+++ terraform /eks.tf
@@ -1,4 +1,3 @@
-
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
@@ -9,16 +8,25 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-
   cluster_endpoint_public_access = true
-  
-  
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]  
-
-
   cluster_endpoint_private_access = true
-
   enable_cluster_creator_admin_permissions = true
 
+  
+  eks_managed_node_groups = {
+    main = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+      
+      instance_types = ["t3.micro"]
+      
+      tags = merge(var.tags, {
+        Name = "${var.cluster_name}-node"
+      })
+    }
+  }
 
+  tags = var.tags
 }
