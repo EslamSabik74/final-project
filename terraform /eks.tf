a7b01b245f02bad1c506db5a2d9689c78e3a5fa1
eslam sabik <eslam@machine1.eslam.com>
2026-02-27 17:52:16 +0200

    update files

--- terraform /eks.tf
+++ terraform /eks.tf
@@ -1,5 +1,6 @@
+
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+  source = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
 
   cluster_name    = var.cluster_name
@@ -8,12 +9,16 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_groups = {
-    main = {
-      instance_types = ["t3.micro"]
-      min_size       = 1
-      max_size       = 1
-      desired_size   = 1
-    }
-  }
+
+  cluster_endpoint_public_access = true
+  
+  
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]  
+
+
+  cluster_endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+
 }
